f398deb60ccf18ec1ff5d88f91c7b66bd226a76b
junghwan.shin <shjohw11@naver.com>
2023-08-27 06:40:46 +0900

    edit

--- src/main/java/com/aihackathon/controller/GptMathController.java
+++ src/main/java/com/aihackathon/controller/GptMathController.java
@@ -8,10 +8,8 @@ import com.aihackathon.service.MathImageConvertService;
 import com.aihackathon.service.TextImageConvertService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.parser.JSONParser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
